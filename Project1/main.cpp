f258c85352ac70c908dbe2d8351d02a94da1948e
Aren Chilingaryan <arenchilingaryan88@gmail.com>
2021-09-30 18:21:51 +0400

    triangle rectangle or square

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,19 +1,68 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 int main() {
 	int height;
-	cout << "Enter haight: ";
+	int length;
+	string what_to_print;
+	cout << "Enter height: ";
 	cin >> height;
+	cout << "Enter length: ";
+	cin >> length;
+	cout << "What to print ? ";
+	cin >> what_to_print;
 
-	for (int i = 1; i <= height; i++) {
-		for (int j = 1; j <= i; j++) {
-			if (j == 1 || j == i || j == height || i == height) {
-				cout << "*";
-			}
-			else {
-				cout << " ";
+	while (true) {
+
+
+		if (what_to_print == "t") {
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j <= i; j++) {
+					if (j == 0 || j == i || j == height - 1 || i == height - 1) {
+						cout << "*";
+					}
+					else {
+						cout << " ";
+					}
+				}
+				cout << endl;
 			}
 		}
-		cout << endl;
+
+		if (what_to_print == "r") {
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < length; j++) {
+					if (i == 0 || i == height - 1 || j == 0 || j == length - 1) {
+						cout << "*";
+					}
+					else {
+						cout << " ";
+					}
+				}
+				cout << endl;
+			}
+		}
+
+		if (what_to_print == "s") {
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < height; j++) {
+					if (i == 0 || i == height - 1 || j == 0 || j == height - 1) {
+						cout << "*";
+					}
+					else {
+						cout << " ";
+					}
+				}
+				cout << endl;
+			}
+
+		}
+		if (what_to_print == "quiet") {
+			break;
+
+		}
+		cin >> what_to_print;
+
 	}
+
 }
