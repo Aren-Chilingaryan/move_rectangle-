0b0042c5ae13ee5a7722ad98701a15e8134c03b8
Aren Chilingaryan <arenchilingaryan88@gmail.com>
2021-09-30 18:59:58 +0400

    rectangle triangle or square with functions

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,62 +1,78 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-int main() {
-	int height;
-	int length;
-	string what_to_print;
-	cout << "Enter height: ";
-	cin >> height;
-	cout << "Enter length: ";
-	cin >> length;
-	cout << "What to print ? ";
-	cin >> what_to_print;
 
-	while (true) {
-		if (what_to_print == "t") {
-			for (int i = 0; i < height; i++) {
-				for (int j = 0; j <= i; j++) {
-					if (j == 0 || j == i || j == height - 1 || i == height - 1) {
-						cout << "*";
-					}
-					else {
-						cout << " ";
-					}
-				}
-				cout << endl;
+void print_triangle(int h) {
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j <= i; j++) {
+			if (j == 0 || j == i || j == h - 1 || i == h - 1) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
 			}
 		}
-
-		if (what_to_print == "r") {
-			for (int i = 0; i < height; i++) {
-				for (int j = 0; j < length; j++) {
-					if (i == 0 || i == height - 1 || j == 0 || j == length - 1) {
-						cout << "*";
-					}
-					else {
-						cout << " ";
-					}
-				}
-				cout << endl;
-			}
-		}
-
-		if (what_to_print == "s") {
-			for (int i = 0; i < height; i++) {
-				for (int j = 0; j < height; j++) {
-					if (i == 0 || i == height - 1 || j == 0 || j == height - 1) {
-						cout << "*";
-					}
-					else {
-						cout << " ";
-					}
-				}
-				cout << endl;
-			}
-		}
-		if (what_to_print == "quiet") {
-			break;
-		}
-		cin >> what_to_print;
+		cout << endl;
 	}
 }
+
+void print_rectangle(int h, int l) {
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < l; j++) {
+			if (i == 0 || i == h - 1 || j == 0 || j == l - 1) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+	void print_square(int h) {
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < h; j++) {
+				if (i == 0 || i == h - 1 || j == 0 || j == h - 1) {
+					cout << "*";
+				}
+				else {
+					cout << " ";
+				}
+			}
+			cout << endl;
+		}
+	}
+
+	int main() {
+		int height;
+		int length;
+		string what_to_print;
+		cout << "Enter height: ";
+		cin >> height;
+		cout << "Enter length: ";
+		cin >> length;
+		cout << "What to print ? ";
+		cin >> what_to_print;
+
+
+		while (true) {
+			if (what_to_print == "t") {
+				print_triangle(height);
+			}
+
+			if (what_to_print == "r") {
+				print_rectangle(height, length);
+			}
+
+			if (what_to_print == "s") {
+				print_square(height);
+			}
+
+			if (what_to_print == "quiet") {
+				break;
+			}
+			cout << "What to print ? ";
+			cin >> what_to_print;
+		}
+	}
