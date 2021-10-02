b52bb8a59493cfaf37ef60143cef749476259971
Aren Chilingaryan <arenchilingaryan88@gmail.com>
2021-10-02 20:21:04 +0400

    rectangle movement

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-#include <cstring>
 #include <Windows.h>
 #include <conio.h>
 #include <iomanip>
-#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
-
-void Clear()
-{
-	cout << "\x1B[2J\x1B[H";
-}
 
 void print_triangle(int side) {
 	for (int i = 0; i < side; i++) {
@@ -25,9 +19,12 @@ void print_triangle(int side) {
 	}
 }
 
-void print_rectangle(int height, int length,int x) {
+void print_rectangle(int height, int length, int quantity_of_spaces, int quantity_of_new_lines) {
+	for (int i = 0; i < quantity_of_new_lines; i++) {
+		cout << endl;
+	}
 	for (int i = 0; i < height; i++) {
-		cout << setw(x);
+		cout << setw(quantity_of_spaces);
 		for (int j = 0; j < length; j++) {
 			if (i == 0 || i == height - 1 || j == 0 || j == length - 1) {
 				cout << "*";
@@ -53,9 +50,14 @@ void print_square(int side) {
 		cout << endl;
 	}
 }
-void clrscr() {};
 
 int main() {
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	int columns, rows;
+
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 
 	int height;
 	int length;
@@ -63,21 +65,38 @@ int main() {
 	cin >> height;
 	cout << "Enter length: ";
 	cin >> length;
-	string area = " ";
-	int x = 5;
+	int quantity_of_spaces = 0;
+	int quantity_of_new_lines = 1;
 
-	print_rectangle(height, length, x);
-
+	print_rectangle(height, length, quantity_of_spaces, quantity_of_new_lines);
 	while (true) {
 		if (GetAsyncKeyState(VK_LEFT)) {
-			Clear();
-			print_rectangle(height, length, x);
-			x = x - 1;
+			quantity_of_spaces -= 1;
+			system("CLS");
+			print_rectangle(height, length, quantity_of_spaces, quantity_of_new_lines);
+			if (quantity_of_spaces < 0)
+				quantity_of_spaces = 0;
 		}
 		else if (GetAsyncKeyState(VK_RIGHT)) {
-			Clear();
-			print_rectangle(height, length, x);
-			x = x + 1;
+			quantity_of_spaces += 1;
+			system("CLS");
+			print_rectangle(height, length, quantity_of_spaces, quantity_of_new_lines);
+			if (quantity_of_spaces > (columns - length))
+				quantity_of_spaces = (columns - length);
+		}
+		else if (GetAsyncKeyState(VK_UP)) {
+			quantity_of_new_lines -= 1;
+			system("CLS");
+			print_rectangle(height, length, quantity_of_spaces, quantity_of_new_lines);
+			if (quantity_of_new_lines < 0)
+				quantity_of_new_lines = 0;
+		}
+		else if (GetAsyncKeyState(VK_DOWN)) {
+			quantity_of_new_lines += 1;
+			system("CLS");
+			print_rectangle(height, length, quantity_of_spaces, quantity_of_new_lines);
+			if (quantity_of_new_lines > rows - height)
+				quantity_of_new_lines = rows - height;
 		}
 	}
 }
